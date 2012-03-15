f90b2814ded4e33d91aa1fcf4e0947fab53c7fb8
veronica <veronica@intersect.org.au>
2012-03-15 15:50:25 +1100

    Added extra comments

--- ands_register/views.py
+++ ands_register/views.py
@@ -51,7 +51,8 @@ def index(request, experiment_id):
     custom_desc = publish_handler.custom_description()
     
     # The following few lines handle the special case of only "<" and ">"
-    # are in the custom description field 
+    # are in the custom description field, which is not handled by the 
+    # "sanitize_html" method in the template 
     if custom_desc == ">":
         c['custom_description'] = "&gt;"
     elif custom_desc == "<":
